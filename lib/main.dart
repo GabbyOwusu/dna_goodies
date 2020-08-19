677358029afe5e0cba1986210f6bc759622452b1
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-08-19 17:02:05 +0100

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DNA Goodies',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Anim(),
