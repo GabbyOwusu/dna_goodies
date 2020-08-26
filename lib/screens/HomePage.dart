45e1282833e9edad886bc23db39a34ac7b5bef42
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2020-08-26 16:54:04 +0100

    Update

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -6,8 +6,149 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  PageController _pageController;
+  // ignore: unused_field
+  int _selectedPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 0, viewportFraction: 0.8);
+  }
+
+  Widget _categories(int index) {
+    return AnimatedBuilder(
+      animation: _pageController,
+      builder: (BuildContext context, Widget widget) {
+        double value = 1;
+        if (_pageController.position.haveDimensions) {
+          value = _pageController.page - index;
+          value = (1 - (value.abs() * 0.3)).clamp(0.0, 1.0);
+        }
+        return Center(
+          child: SizedBox(
+            height: Curves.easeInOut.transform(value) * 430.0,
+            width: Curves.easeInOut.transform(value) * 400.0,
+            child: widget,
+          ),
+        );
+      },
+      child: GestureDetector(
+        onTap: () {},
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(251, 174, 40, 1),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 30.0),
+              child: Stack(
+                children: <Widget>[
+                  Center(child: Icon(Icons.home)),
+                  // Positioned(
+                  //   top: 30.0,
+                  //   right: 30.0,
+                  //   child: Column(
+                  //     crossAxisAlignment: CrossAxisAlignment.center,
+                  //     children: <Widget>[
+
+                  //     ],
+                  //   ),
+                  // ),
+                  Positioned(
+                    left: 30.0,
+                    bottom: 40.0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Type',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Text(
+                          'Category Name',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(
+          'LOGO',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+            icon: Icon(Icons.menu), color: Colors.black, onPressed: () {}),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add_shopping_cart),
+            color: Colors.black,
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 20, top: 20),
+                child: Text(
+                  'Categories',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: 'SanFransisco',
+                  ),
+                ),
+              ),
+              Container(
+                height: 500,
+                child: PageView.builder(
+                  physics: BouncingScrollPhysics(),
+                  controller: _pageController,
+                  onPageChanged: (int index) {
+                    setState(() {
+                      _selectedPage = index;
+                    });
+                  },
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 4,
+                  itemBuilder: (context, index) => _categories(index),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
